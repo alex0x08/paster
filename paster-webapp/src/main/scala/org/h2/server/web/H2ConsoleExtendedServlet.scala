1eee9041ed586b0e8256aa7b4ea942a71f7f6189
Alex (illuminati) <alex3.145@gmail.com>
2016-04-27 17:26:22 +0800

    h2 console servlet

--- paster-webapp/src/main/scala/org/h2/server/web/H2ConsoleExtendedServlet.scala
+++ paster-webapp/src/main/scala/org/h2/server/web/H2ConsoleExtendedServlet.scala
@@ -1,121 +1,195 @@
-/*
- * Copyright 2014 Ubersoft, LLC.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package org.h2.server.web
 
 import java.sql.SQLException
+import java.util.HashMap
 import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletRequestWrapper
 import javax.servlet.http.HttpServletResponse
 import javax.sql.DataSource
 import org.springframework.context.ApplicationContext
 import org.springframework.web.context.support.WebApplicationContextUtils
 import uber.paste.base.Loggered
-
+import scala.collection.JavaConversions._
 /**
- * This is extended version of JDBC Database Console Servlet, 
+ * This is extended version of JDBC Database Console Servlet,
  * packed with H2 Driver
- * 
- * It was created to automatically bind connection from local datasource 
+ *
+ * It was created to automatically bind connection from local datasource
  * to user's session and open console automatically.
- * 
+ *
  */
-class H2ConsoleExtendedServlet extends WebServlet with Loggered{
+class H2ConsoleExtendedServlet extends WebServlet with Loggered {
 
-  private var dataSource:DataSource = null
-  
+  private var dataSource: DataSource = null
+
   private val server = new WebServer
-  
-   override def init() {         
-        
+
+  override def init() {
+
     logger.debug("h2 servlet init..")
-    
-       val ctx:ApplicationContext = WebApplicationContextUtils
-                    .getWebApplicationContext(getServletContext())
-       
-      dataSource = ctx.getBean("dataSource").asInstanceOf[DataSource]
-       
-         try {
-                server.setAllowChunked(false)
-                server.init()
-         
-           val  f =getClass().getSuperclass().getDeclaredField("server")
-                    f.setAccessible(true)       
-                    f.set(this, server)
-               
-           //    val session = createAndRegLocalSession()
-                
-            //   val ss = session.get("sessionId").asInstanceOf[String]
-                
-            //  logger.debug("h2 sessionId: {}",ss);
-                
-           //  getServletContext().setAttribute("h2console-session-id",ss);   
-            
-       } catch {
-           case e @ (_ : NoSuchFieldException 
-                     | _ : IllegalAccessException
-                     | _ : SQLException
-                     | _ : SecurityException                    
-                     | _ : IllegalArgumentException) => {    
-               logger.error(e.getLocalizedMessage,e)
-                }
-          }  
-       
-     }
-     
+
+    val ctx: ApplicationContext = WebApplicationContextUtils
+      .getWebApplicationContext(getServletContext())
+
+    dataSource = ctx.getBean("dataSource").asInstanceOf[DataSource]
+
+    try {
+      server.setAllowChunked(false)
+      server.init()
+
+      val f = getClass().getSuperclass().getDeclaredField("server")
+      f.setAccessible(true)
+      f.set(this, server)
+
+      //    val session = createAndRegLocalSession()
+
+      //   val ss = session.get("sessionId").asInstanceOf[String]
+
+      //  logger.debug("h2 sessionId: {}",ss);
+
+      //  getServletContext().setAttribute("h2console-session-id",ss);   
+
+    } catch {
+      case e @ (_: NoSuchFieldException
+        | _: IllegalAccessException
+        | _: SQLException
+        | _: SecurityException
+        | _: IllegalArgumentException) => {
+        logger.error(e.getLocalizedMessage, e)
+      }
+    }
+
+  }
+
   @throws(classOf[java.io.IOException])
-  override def doGet(req:HttpServletRequest,res:HttpServletResponse) {
-  
-    var session =  server.getSession(
-      getServletContext().getAttribute("h2console-session-id").asInstanceOf[String])
-  
-    if (session==null) {
+  override def doGet(req: HttpServletRequest, res: HttpServletResponse) {
+
+    
+    val h2console_db_session_id = getH2SessionIdFromContext() 
+
+    var session: WebSession = null
+
+    if (h2console_db_session_id != null) {
+
+      session = server.getSession(h2console_db_session_id)
+
+      if (session == null) {
+        session = createAndRegLocalSession()
+      } else {
+        logger.debug("using existing session {}",h2console_db_session_id)        
+      }
+
+    } else {
       session = createAndRegLocalSession()
     }
-             
-    
+
     /**
      * refresh connection if closed
+
      */
     var con = session.getConnection
-    
-    if (con==null ||con.isClosed) {
-      con =dataSource.getConnection
-      
+
+    if (con == null || con.isClosed) {
+      con = dataSource.getConnection
+
       session.setConnection(con)
-      session.put("url",con.getMetaData.getURL)
+      session.put("url", con.getMetaData.getURL)
+      logger.debug("reopened connection to db")
     }
     
-    super.doGet(req,res)
+    
+    
+    /**
+     *  attributes are filled from both httpservlet attributes and params
+     * so easy solution like
+     * 
+     *   //req.g.setAttribute("jsessionid", h2console_db_session_id)
+    
+          will not work
+     * 
+     *  WebSession session = null;
+        String sessionId = attributes.getProperty("jsessionid");
+        if (sessionId != null) {
+            session = server.getSession(sessionId);
+        }
+     */
+     
+    
+     
+    val map = new HashMap[String,String]()
+    map.put("jsessionid",getH2SessionIdFromContext() )
+    
+    super.doGet(new ExtendedHttpServletRequestWrapper(req,map), res)
+  }
+
+  private def getH2SessionIdFromContext() = getServletContext()
+      .getAttribute("h2console-session-id").asInstanceOf[String]
+  
+  private def  createAndRegLocalSession(): WebSession = {
+
+    this.synchronized {
+      
+    
+      val conn = dataSource.getConnection()
+
+    val session = server.createNewSession("local")
+    session.setShutdownServerOnDisconnect()
+    session.setConnection(conn)
+    session.put("url", conn.getMetaData().getURL())
+
+    /**
+     *
+     *  sessionId here is INTERNAL id, not container's jsessionId
+     *
+     * WebSession createNewSession(String hostAddr) {
+     * String newId;
+     * do {
+     * newId = generateSessionId();
+     * } while (sessions.get(newId) != null);
+     * WebSession session = new WebSession(this);
+     * session.lastAccess = System.currentTimeMillis();
+     * session.put("sessionId", newId);
+     *
+     */
+
+    val ss = session.get("sessionId").asInstanceOf[String]
+
+    logger.debug("created h2 session {}", ss)
+
+    getServletContext().setAttribute("h2console-session-id", ss)
+
+        return session
+    }
+    
+    
+  
   }
   
-  private def createAndRegLocalSession():WebSession = {
+  
+  class ExtendedHttpServletRequestWrapper(req:HttpServletRequest, params:java.util.Map[String,String]) 
+            extends HttpServletRequestWrapper(req) {
+
     
-     val conn = dataSource.getConnection()
-                
-               val session = server.createNewSession("local")
-                   session.setShutdownServerOnDisconnect()
-                   session.setConnection(conn)
-                   session.put("url", conn.getMetaData().getURL())
-        
-               val ss = session.get("sessionId").asInstanceOf[String]
-                
-              logger.debug("h2 sessionId: {}",ss)
-              
-              getServletContext().setAttribute("h2console-session-id",ss)  
+           
     
-              return session
+    override def getParameter(name:String):String = {
+     
+           // if we added one, return that one
+          val out = if ( params.containsKey( name ) ) {
+                 params.get( name )
+           } else {
+           // otherwise return what's in the original request
+           super.getParameter( name ) 
+             
+           }
+           
+       System.out.println("name="+name+" val="+out)
+       return out
+   }
+
+   
+ 
   }
+
 }
